400206d1aeebb40bc340e367af8be21ed5864f5f
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-04-09 16:03:05 -0400

    Uso de los nuevos componentes en la presentacion de crypto app

---
+++ src/features/crypto-demo/screens/CryptoIntroScreen.tsx
@@ -0,0 +1,25 @@
+import { ButtonProjects } from '@/src/shared/components/ButtonProjects';
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { HeaderIntro } from '../components/crypto-intro/HeaderIntro';
+import { TechIntro } from '../components/crypto-intro/TechIntro';
+
+export const CryptoIntroScreen = () => {
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      {/* Header de la intro */}
+      <HeaderIntro/>
+
+      {/* Body de la intro */}
+      <TechIntro/>
+
+      {/* Boton para ir a la pantalla principal */}
+      <ButtonProjects routeLink={'/flows/crypto-app/main-crypto'} titleBtn='Ver Demo en Vivo'/>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { padding: 24, alignItems: 'center' },
+});
