e1516c66453dfc70e51ea2981c1d55cdeabb38b2
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-09 14:37:38 -0400

    Configuracion del layout de la demo crypto

--- app/flows/_layout.tsx
+++ app/flows/_layout.tsx
@@ -1,19 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+
+const queryClient = new QueryClient();
 
 export default function FlowsLayout() {
   return(
-    <Stack
-      screenOptions={{
-        headerShown: true,
-        animation: 'slide_from_right',
-        headerTitleAlign: 'center',
-      }}
-    >
-      <Stack.Screen
-        name="crypto/(tabs)"
-        options={{title: 'Crypto-Dashboard'}}
-      />
-
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack
+        screenOptions={{
+          headerShown: true,
+          animation: 'slide_from_right',
+          headerTitleAlign: 'center',
+        }}
+      >
+        <Stack.Screen
+          name="crypto/(tabs)"
+          options={{title: 'Crypto-Dashboard'}}
+        />
+      </Stack>
+    </QueryClientProvider>
   )
 }
