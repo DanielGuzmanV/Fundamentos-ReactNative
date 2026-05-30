56787c2331a5e9cb3b65013b3b95bdf10bfde2d3
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-30 17:36:12 -0400

    Se quito la actualizacion automatica cada 30 segundos

--- src/features/crypto-demo/main/hooks/useWatchlistData.ts
+++ src/features/crypto-demo/main/hooks/useWatchlistData.ts
@@ -9,6 +9,6 @@ export const useWatchlistData = () => {
     queryFn: () => getCoincapAssets(WATCHLIST_IDS),
     // Opciones de React Query
     staleTime: 1000 * 60 * 5, // Los datos se consideran "frescos" por 5 minutos
-    refetchInterval: 1000 * 30, // Refrescar cada 30 segundos en segundo plano
+    // refetchInterval: 1000 * 30, // Refrescar cada 30 segundos en segundo plano
   });
 };
