fd403439c5659112b3b639473d865c07d51d2280
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-30 18:27:00 -0400

    Se agregaron codigo temporal para las pruebas

--- src/features/crypto-demo/main/hooks/useWatchlistData.ts
+++ src/features/crypto-demo/main/hooks/useWatchlistData.ts
@@ -6,6 +6,19 @@ import { CoinData } from '../types/coinCap';
 export const useWatchlistData = () => {
   return useQuery<CoinData[], Error>({
     queryKey: ['watchlist'], // Clave única para esta petición
+
+    //--- CÓDIGO TEMPORAL PARA PROBAR ERROR WATCHLIST ---
+    // queryFn: async () => {
+    //   await new Promise(resolve => setTimeout(resolve, 1000)); // Simula un pequeño retraso
+    //   throw new Error("¡Simulación de error de red o de API!");
+    // },
+
+    //--- CÓDIGO TEMPORAL PARA PROBAR EMPTY WATCHLIST ---
+    // queryFn: async () => {
+    //   await new Promise(resolve => setTimeout(resolve, 1000)); // Simula un pequeño retraso
+    //   return [];
+    // },
+
     queryFn: () => getCoincapAssets(WATCHLIST_IDS),
     // Opciones de React Query
     staleTime: 1000 * 60 * 5, // Los datos se consideran "frescos" por 5 minutos
