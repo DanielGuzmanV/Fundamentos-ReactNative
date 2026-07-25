5026c83aaed3030abd817517d4e623978fb7f7fe
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-07-25 18:04:00 -0400

    Pantalla principal refactorizada y ajustada

---
+++ src/features/home/screens/HomeScreen.tsx
@@ -0,0 +1,66 @@
+import { TextCustom, ViewCustom } from "@/src/shared/components/Themed";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { ConceptCard } from "../components/ConceptCard";
+import { CONCEPTS_DATA } from "../data/concepts.data";
+import { ConceptDifficulty } from "../types/concept.types";
+
+export const HomeScreen = () => {
+  const handleSelectLevel = (levelId: ConceptDifficulty) => {
+    console.log('Nivel seleccionado:', levelId);
+  }
+
+  return (
+    <ViewCustom style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false}
+      >
+        {/* Header principal */}
+        <View style={styles.header}>
+          <TextCustom style={styles.title}>Conceptos React Native</TextCustom>
+          <TextCustom style={styles.subtitle}>
+            Ruta modular de aprendizaje. Selecciona un nivel para dominar sus fundamentos y patrones de código.
+          </TextCustom>
+        </View>
+
+        {/* Seccion de cards */}
+        <View style={styles.cardsContainer}>
+          {CONCEPTS_DATA.map((level) => (
+            <ConceptCard
+              key={level.id}
+              level={level}
+              onPress={handleSelectLevel}
+            />
+          ))}
+        </View>
+
+      </ScrollView>
+    </ViewCustom>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1,
+  },
+  scrollContent: {
+    padding: 20,
+    paddingBottom: 40,
+  },
+  header: { 
+    marginBottom: 24,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: '800',
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 14,
+    opacity: 0.7,
+    lineHeight: 20,
+  },
+  cardsContainer: {
+    gap: 4,
+  }
+})
