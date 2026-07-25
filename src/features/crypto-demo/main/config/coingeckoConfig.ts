74e8ceb1eeb9d4f0c5763346692e90eba3fa380b
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-07-25 15:37:19 -0400

    Configuracion de la url y api key de coingecko

---
+++ src/features/crypto-demo/main/config/coingeckoConfig.ts
@@ -0,0 +1,5 @@
+// URL de coingecko:
+export const COINGECKO_API_BASE_URL = 'https://api.coingecko.com/api/v3/';
+
+// API key:
+export const COINGECKO_API_KEY = process.env.EXPO_PUBLIC_COINGECKO_API_KEY;
