a9dc2ee4616ffefedbd4534c4fa6d461628d31b5
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-04 17:34:49 -0400

    Se crearon las rutas para cada tipo de concepto

---
+++ app/flows/concepts/advanced/index.tsx
@@ -0,0 +1,6 @@
+import AdvancedConceptScreen from '@/src/features/home/screens/AdvancedConceptScreen';
+
+export default function ConceptAdvanced() {
+  return <AdvancedConceptScreen/>
+}
+
---
+++ app/flows/concepts/intermediate/index.tsx
@@ -0,0 +1,5 @@
+import IntermediateConceptScreen from '@/src/features/home/screens/IntermediateConceptScreen';
+
+export default function ConceptIntermediate() {
+  return <IntermediateConceptScreen/>
+}
