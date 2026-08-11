fa9bfdae2c664a28fe0f3b3bab09c21946bd3a55
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-11 18:16:11 -0400

    Se creo un helper para los colores de cada concepto

---
+++ src/features/home/helper/colors.ts
@@ -0,0 +1,7 @@
+import { ConceptDifficulty } from "../types/concept.types";
+
+export const LEVEL_COLORS: Record<ConceptDifficulty, string> = {
+  basic: '#34C759',
+  intermediate: '#FF9500',
+  advanced: '#FF3B30',
+}
