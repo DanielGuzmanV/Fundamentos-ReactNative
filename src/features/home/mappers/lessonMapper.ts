274641826a73a50dab56e9cbcaff0b14a0a3ceef
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-30 22:30:24 -0400

    refactor(home): agreron la nueva pantalla de conceptos basicos al mapper

---
+++ src/features/home/mappers/lessonMapper.ts
@@ -0,0 +1,12 @@
+import { ComponentType } from "react";
+import JsxMobileScreen from "../screens/lessons/basic/JsxMobileScreen";
+
+export const LESSON_COMPONENTS: Record<string, ComponentType> = {
+  // lecciones de conceptos basicos:
+  "jsx": JsxMobileScreen
+  
+  // Lecciones de conceptos intermedios:
+
+  // Lecciones de conceptos avanzados:
+
+}
