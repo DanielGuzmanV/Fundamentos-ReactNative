9622e0173e113ffd5a6146ab583bbce1f411644f
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-30 22:21:00 -0400

    feat(home-lessons): creacion de una data con pocos elementos para comparacion

---
+++ src/features/home/constants/list_label.ts
@@ -0,0 +1,5 @@
+export const LIST_LABEL = [
+  { labelWeb: '<div>', labelMobile: '<View>' },
+  { labelWeb: '<p> / <span>', labelMobile: '<Text>' },
+  { labelWeb: '<button>', labelMobile: '<Pressable>' },
+]
