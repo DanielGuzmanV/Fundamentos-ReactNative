6dec05f9ca0ae6bdc0aae08252b9812ba7ab22ff
DLGuzBuho <danielguzmanbuho23@gmail.com>
2026-09-04 14:37:13 -0400

    refactor(mappers): se agrego la ruta para la leccion de componentes

--- src/features/home/mappers/lessonMapper.ts
+++ src/features/home/mappers/lessonMapper.ts
@@ -1,9 +1,11 @@
 import { ComponentType } from "react";
+import ComponentsScreen from "../screens/lessons/basic/ComponentsScreen";
 import JsxMobileScreen from "../screens/lessons/basic/JsxMobileScreen";
 
 export const LESSON_COMPONENTS: Record<string, ComponentType> = {
   // lecciones de conceptos basicos:
-  "jsx": JsxMobileScreen
+  "jsx": JsxMobileScreen,
+  "components": ComponentsScreen
   
   // Lecciones de conceptos intermedios:
 
